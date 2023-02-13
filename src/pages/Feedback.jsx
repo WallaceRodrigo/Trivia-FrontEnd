03bbae4f1974cafa53ce472008a729e939030b15
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2023-02-13 16:19:19 -0300

    req16

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -27,6 +27,12 @@ class Feedback extends Component {
         >
           Play Again
         </button>
+        <button
+          data-testid="btn-ranking"
+          onClick={ () => history.push('/ranking') }
+        >
+          Ranking
+        </button>
       </div>
     );
   }
